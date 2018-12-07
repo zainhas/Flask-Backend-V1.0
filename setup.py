05b5f14311b9ea29ad6fb64fc11266de1d2f99de
Pratik Agrawal <pratik.agrawal@bresotec.com>
2018-12-06 20:31:14 -0500

    [WIP] Project restructured

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='yourapplication',
+    packages=['yourapplication'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+        'flask-restful',
+        'flask-sqlalchemy'
+    ],
+)
